3a08f4abc940d69ff41184cd83ce3bf0266d5c82
oky31 <saputraoky6@gmail.com>
2017-12-20 11:41:14 +0700

    conversi warna menjadi rgb

--- blur.py
+++ blur.py
@@ -11,12 +11,15 @@ def main():
     blur = cv2.blur(citra,(7,7))
 
     # menampilkan citra
-    plt.subplot(121),plt.imshow(citra),plt.title("Gambar Asli")
+    plt.subplot(121)
+    plt.imshow(cv2.cvtColor(citra,cv2.COLOR_BGR2RGB))
+    plt.title("Gambar Asli")
     plt.xticks([]),plt.yticks([])
 
-    plt.subplot(122),plt.imshow(blur),plt.title("Gambar Blur")
+    plt.subplot(122),plt.imshow(cv2.cvtColor(blur,cv2.COLOR_BGR2RGB))
+    plt.title("Gambar Blur")
     plt.xticks([]),plt.yticks([])
-    
+
     plt.show()
 
 if __name__ == '__main__':
